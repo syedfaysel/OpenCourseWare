267573a4b7398914185eaf1fd28e13c8f7ef383e
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2022-07-22 16:24:57 +0600

    upgrading with json

---
+++ index.js
@@ -0,0 +1,15 @@
+import data from "./assets/data.json" assert {type: 'json'};
+
+
+
+console.log(data)
+
+const prefix = "https://www.youtube.com/embed/videoseries?list=";
+const source = document.getElementById("video-player").src
+document.getElementById("video-player").src = prefix+data.videoId.Skills.cpp[0];
+
+
+var objectLength = Object.keys(data.videoId.Skills).length;
+for(var i=0;i<=objectLength;i++){
+    console.log("hi")
+}
