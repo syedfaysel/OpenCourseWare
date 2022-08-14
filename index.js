1165574337b4273016590cd6f5ccf8b97da3a189
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2022-08-14 09:28:36 +0600

    algo

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import data from "./assets/data.json" assert {type: 'json'};
 
 
 
-console.log(data)
+// console.log(data)
 
 const prefix = "https://www.youtube.com/embed/videoseries?list=";
-const source = document.getElementById("video-player").src
-document.getElementById("video-player").src = prefix+data.videoId.cse.phy112[0];
+// const source = document.getElementById("video-player").src
+document.getElementById("video-player").src = prefix+data.videoId.Skills.algo[0];
 
 
 var objectLength = Object.keys(data.videoId.Skills).length;
