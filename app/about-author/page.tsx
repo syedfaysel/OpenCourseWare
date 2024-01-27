e98512c758ae3807032182350ad4d2f6ce83c857
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2024-01-28 00:29:32 +0600

    boom

---
+++ app/about-author/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl">This is Syed Faysel Ahammad Rajo</h1>
+      <p>See you around. This project is under construction</p>
+    </main>
+  );
+}
