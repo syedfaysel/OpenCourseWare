b3e27c9e07c629ab3e225d5e40e0cff2aec08e9b
DEF4LT-303 <ryanrafi72@gmail.com>
2024-03-26 00:00:29 +0600

    maxwidth wrapper and coursecode on image

--- src/app/components/CoursesContainer.tsx
+++ src/app/components/CoursesContainer.tsx
@@ -7,15 +7,17 @@ const CoursesContainer =  ({courses}: any) => {
 
   return (
     <>
-      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4 my-5 mx-5">
+    <div className="max-w-[1600px] mx-auto">
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-5 my-10 mx-5">
           {courses?.map((course: any) => {
             return (
               <div key={course.courseCode} className="">
-                  <CourseCard courseCode={course.courseCode} title={course.title} imageUrl={course.imageUrl} courseType={course.courseType} courseCategory={course.courseCategory} />
+                  <CourseCard courseCode={course.courseCode} title={course.title} courseCategory={course.courseCategory} />
               </div>
             );
           })}
       </div>
+    </div>
     </>
   );
 };
