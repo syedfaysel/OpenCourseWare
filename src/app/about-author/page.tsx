6b450f3b5482615516634e86736328d60cde0fc7
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2024-09-27 15:55:12 +0600

    retouch

--- src/app/about-author/page.tsx
+++ src/app/about-author/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <main className="flex min-h-[70vh] flex-col items-center justify-center">
       <h1 className="text-3xl mb-2">This is Syed Faysel Ahammad Rajo</h1>
       <p>See you around! This project is under construction</p>
-      <p>Meanwhile you can explore <span><Link href='/coures' className='text-yellow-300'>courses</Link></span></p>
+      <p>Meanwhile you can explore <span><Link href='/courses' className='text-yellow-300'>courses</Link></span></p>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center py-2 min_height">
+    <main className="flex flex-col items-center justify-center py-2 min_height text-center">
       <h1 className="text-3xl font-bold text-center p-6 lg:text-4xl">
         Hello Folks! <br /> Welcome to <span className="text-yellow-500">ocw</span>X
       </h1>
+      <p>The editors are working on the website Meanwhile you can </p>
+      <span className="bg-blue-400 m-1 p-2 rounded hover:bg-blue-600"><Link href='/courses' className='font-bold text-black'>explore courses</Link></span>
       
     </main>
   );
